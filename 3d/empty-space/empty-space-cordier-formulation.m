7395422bc743c90c7350afccebeab3b0ccbb6db2
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-01-17 17:38:57 -0700

    Added 'empty-space' directory to consolidate related files

---
+++ 3d/empty-space/empty-space-cordier-formulation.m
@@ -0,0 +1,18 @@
+%The ffmatlib library doesn't seem to work out of the box with non-Lagrangian elements.
+%TODO: See if Matlab / Octave support Nedelec elements and implement support in ffmatlib
+clear all;
+addpath('../matlab/release-v2.0/demos/ffmatlib');
+
+%This only works for medit mesh formats...
+%[pts, bdy_tris, tetra, edges, quads, hexahedra, num_pts, num_tetra, num_bdy_tris] = ffreadmesh('empty-space-mesh.mesh');
+[pts, bdy_edges, tris, num_pts, num_bdy_edges, num_tris, labels] = ffreadmesh('empty-space-mesh.mesh');
+[fespace] = ffreaddata('empty-space-nedelec-fespace.txt');
+[Bx, By, Bz] = ffreaddata('empty-space-magnetic-field.txt');
+[Ax, Ay, Az] = ffreaddata('empty-space-vector-potential.txt');
+
+figure;
+ffpdeplot3D(pts, bdy_edges, tris, 'VhSeq', fespace, ...
+    'FlowData', [Bx, By, Bz]);
+ylabel('y');
+xlabel('x');
+zlabel('z');
